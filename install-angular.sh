df5559ad9fd758c1259c8da30a17d786e6c65a9d
igor2000xp <igorring@gmail.com>
2023-10-11 08:15:58 +0200

    initial commit

---
+++ install-angular.sh
@@ -0,0 +1,15 @@
+#bin/bash
+ng version
+ng new angular-test-16-02 --directory ./
+
+npm install @angular/material @angular/cdk
+
+
+ng add @angular-eslint/schematics
+
+npm install eslint-plugin-jasmine --save-dev
+
+npm install eslint-config-airbnb-typescript eslint-plugin-import --save-dev
+npm i prettier eslint-config-prettier eslint-plugin-prettier --save-dev
+
+# npx eslint --init
