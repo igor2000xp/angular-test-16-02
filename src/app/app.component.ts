4cf4f9b41c45d2523ed2d40e97da69b4bec1ef74
igor2000xp <igorring@gmail.com>
2023-10-13 13:49:47 +0200

    feat: use the ngDoCheck and ngOnChanges lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss'],
+// })
+// export class AppComponent {
+//   title = 'angular-test-16-02';
+// }
+
+//our root app component
+import { Component, NgModule, ChangeDetectionStrategy, ChangeDetectorRef, Input, VERSION } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+@Component({
+  selector: 'a-comp',
+  template: `<h2>The name is: {{o.name}} </h2>`,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AComponent {
+  @Input() o: { id: any; name: any } = { id: 0, name: '' };
+  id: any;
+
+  constructor(private cd: ChangeDetectorRef) { }
+
+  ngDoCheck() {
+    if (this.id !== this.o.id) {
+      this.cd.markForCheck();
+      this.id = this.o.id;
+    }
+  }
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <h4>Hello {{name}}</h4>
+    <a-comp [o]="o"></a-comp>
+  `,
 })
-export class AppComponent {
-  title = 'angular-test-16-02';
+export class App {
+  name = `Angular! v${VERSION.full}`;
+  o = { id: 1, name: 'John' };
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.o.id = 2;
+      this.o.name = 'Jane';
+    }, 2000);
+  }
 }
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [App, AComponent],
+  bootstrap: [App]
+})
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// import { AppModule } from './app/app.module';
+
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+//main entry point
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.component';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
