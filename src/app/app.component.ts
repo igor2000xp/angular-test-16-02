f99c5d30c41d3997ccef45ed909e9480f40ea174
igor2000xp <igorring@gmail.com>
2023-10-14 10:51:06 +0200

    feat: run setTimeout outside Angular zone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss'],
+// })
+// export class AppComponent {
+//   title = 'angular-test-16-02';
+// }
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+      <h3>
+          Change detection is triggered at:
+          <span [textContent]="time | date:'hh:mm:ss:SSS'"></span>
+      </h3>
+      <button class="btn btn-primary" (click)="0">Trigger Change Detection</button>
+      <p>Examle is here:</p>
+      <a href="https://stackblitz.com/edit/angular-c5sr8u?file=src%2Fapp%2Fapp.component.ts">stackblitz</a>
+  `
 })
 export class AppComponent {
-  title = 'angular-test-16-02';
+  _time;
+  get time() {
+    return this._time;
+  }
+
+  constructor(zone: NgZone) {
+    this._time = Date.now();
+
+    zone.runOutsideAngular(() => {
+      setInterval(() => {
+        this._time = Date.now()
+      }, 1);
+    });
+  }
 }
